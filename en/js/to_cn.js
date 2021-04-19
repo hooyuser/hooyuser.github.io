1f6e94f84d2350e30ab0457077095e1eae4fcaf7
hooyuser <hooyuser@outlook.com>
2021-04-20 04:27:32 +0800

    Site updated: 2021-04-20 04:27:30

---
+++ en/js/to_cn.js
@@ -0,0 +1,16 @@
+var btn = document.createElement('a');
+let language = {};
+language.now = location.pathname.match(/^\/en/) ? 'en' : 'cn';
+if ('en' === language.now) {
+    language.label = '中文';
+    language.href = location.pathname.replace(/^\/en/, '/cn');
+} else {
+    language.label = 'English';
+    language.href = location.pathname.replace(/^\/cn/, '/en');
+}
+btn.innerHTML = `<li class="nav-item">
+    <a class="nav-link" href="${language.href}">
+    ${language.label}
+    </a>
+    </li>`
+$("#navbarSupportedContent")[0].childNodes[1].append(btn);
