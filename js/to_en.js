06d1ea8b0ad350cc2673ad6ca77c5bfa010045d1
hooyuser <hooyuser@outlook.com>
2021-04-20 03:54:41 +0800

    Site updated: 2021-04-20 03:54:13

---
+++ js/to_en.js
@@ -0,0 +1,14 @@
+let language = {};
+language.now = location.pathname.match(/^\/en/) ? 'en' : 'cn';
+if ('en' === language.now) {
+    language.label = '中文';
+    language.href = location.pathname.replace(/^\/en/, '/cn');
+} else {
+    language.label = 'English';
+    language.href = location.pathname.replace(/^\/cn/, '/en');
+}
+$("#navbarSupportedContent")[0].childNodes[1].append(`<li class="nav-item">
+<a class="nav-link" href="${language.href}">
+  ${language.label}
+</a>
+</li>`);
