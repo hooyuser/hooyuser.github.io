2bbf9ac48a4a56b4b2f394421cb6b6759681f88a
hooyuser <hooyuser@outlook.com>
2021-04-20 04:22:09 +0800

    Site updated: 2021-04-20 04:22:05

--- js/to_en.js
+++ js/to_en.js
@@ -1,18 +1,16 @@
-$(window).on('load', function(){
-    var btn = document.createElement('button');
-    let language = {};
-    language.now = location.pathname.match(/^\/en/) ? 'en' : 'cn';
-    if ('en' === language.now) {
-        language.label = '中文';
-        language.href = location.pathname.replace(/^\/en/, '/cn');
-    } else {
-        language.label = 'English';
-        language.href = location.pathname.replace(/^\/cn/, '/en');
-    }
-    btn.innerHTML = `<li class="nav-item">
+var btn = document.createElement('button');
+let language = {};
+language.now = location.pathname.match(/^\/en/) ? 'en' : 'cn';
+if ('en' === language.now) {
+    language.label = '中文';
+    language.href = location.pathname.replace(/^\/en/, '/cn');
+} else {
+    language.label = 'English';
+    language.href = location.pathname.replace(/^\/cn/, '/en');
+}
+btn.innerHTML = `<li class="nav-item">
     <a class="nav-link" href="${language.href}">
     ${language.label}
     </a>
     </li>`
-    $("#navbarSupportedContent")[0].childNodes[1].append(btn);
-});
+$("#navbarSupportedContent")[0].childNodes[1].append(btn);
